bb455f753a7b1ff44bc7be2685b3fabe71f86068
Denys Karpov <Jeremymeme@users.noreply.huggingface.co>
2023-08-27 20:09:33 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,24 +5,15 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma, LanceDB
 from langchain.chains import RetrievalQA
 
-def get_api_key():
-    # Check if the API key is already stored in the session state
-    api_key = os.environ.get('OPENAI_API_KEY')
-    if api_key:
-        return api_key
-        
-    if 'OPENAI_API_KEY' in st.session_state:
-        return st.session_state['OPENAI_API_KEY']
-    
-    # Prompt the user to enter the API key
+
+
+ if 'OPENAI_API_KEY' in st.session_state:
+    api_key = st.session_state['OPENAI_API_KEY']
+ else:
     api_key = st.text_input('Enter your API key', type='password')
-    os.environ['OPENAI_API_KEY'] = api_key
-    
-    # Store the API key in the session state
     st.session_state['OPENAI_API_KEY'] = api_key
-    
-    # Return the API key
-    return api_key
+
+
 
 def generate_response(uploaded_file, openai_api_key, query_text):
     # Load document if file is uploaded
@@ -32,21 +23,20 @@ def generate_response(uploaded_file, openai_api_key, query_text):
         text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         texts = text_splitter.create_documents(documents)
         # Select embeddings
-        embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get('OPENAI_API_KEY'))
+        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
         # Create a vectorstore from documents
         db = Chroma.from_documents(texts, embeddings)
         # Create retriever interface
         retriever = db.as_retriever()
         # Create QA chain
-        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=os.environ.get('OPENAI_API_KEY')), chain_type='stuff', retriever=retriever)
+        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key, chain_type='stuff', retriever=retriever)
         return qa.run(query_text)
 
 # Page title
 st.set_page_config(page_title='🦜🔗 TextyTalk')
 st.title('🦜🔗 TextyTalk')
 
-openai_api_key = get_api_key()
-print(openai)
+
 # File upload
 uploaded_file = st.file_uploader('Upload a document', type='txt')
 # Query text
@@ -61,7 +51,7 @@ with st.form('myform', clear_on_submit=True):
     submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
     if submitted:
         with st.spinner('Calculating...'):
-            response = generate_response(uploaded_file, os.environ.get('OPENAI_API_KEY'), query_text)
+            response = generate_response(uploaded_file, st.session_state['OPENAI_API_KEY'], query_text)
             result.append(response)
 
 if len(result):
