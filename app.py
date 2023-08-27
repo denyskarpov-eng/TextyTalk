199f59973e00addfbd8d9402ca7fe7ee3d86ae99
Denys Karpov <Jeremymeme@users.noreply.huggingface.co>
2023-08-27 14:05:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,20 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma, LanceDB
 from langchain.chains import RetrievalQA
 
+def get_api_key():
+    # Check if the API key is already stored in the session state
+    if 'api_key' in st.session_state:
+        return st.session_state['api_key']
+    
+    # Prompt the user to enter the API key
+    api_key = st.text_input('Enter your API key', type='password', disabled=not (uploaded_file and query_text))
+    
+    # Store the API key in the session state
+    st.session_state['api_key'] = api_key
+    
+    # Return the API key
+    return api_key
+
 def generate_response(uploaded_file, openai_api_key, query_text):
     # Load document if file is uploaded
     if uploaded_file is not None:
@@ -33,8 +47,10 @@ query_text = st.text_input('Enter your question:', placeholder = 'Please provide
 
 # Form input and query
 result = []
+openai_api_key = get_api_key()
+
 with st.form('myform', clear_on_submit=True):
-    openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+    #openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
     submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
     if submitted and openai_api_key.startswith('sk-'):
         with st.spinner('Calculating...'):
