b3c4da47b5d2573f5cd13532189c0396ddfe5428
Denys Karpov <Jeremymeme@users.noreply.huggingface.co>
2023-09-07 14:48:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -58,14 +58,13 @@ query_text = st.text_input('Enter your question:', placeholder = 'Please provide
 result = []
 
 
-with st.form('myform', clear_on_submit=True):
-    #openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+#with st.form('myform', clear_on_submit=True):
     #submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
     #if submitted:
-    if uploaded_file and query_text:
-        with st.spinner('Calculating...'):
-            response = generate_response(uploaded_file, user_input, query_text)
-            result.append(response)
+if uploaded_file and query_text:
+    with st.spinner('Calculating...'):
+        response = generate_response(uploaded_file, user_input, query_text)
+        result.append(response)
 
 if len(result):
     st.info(response)
