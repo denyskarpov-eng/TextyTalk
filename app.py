1c0b5d081a0c79588c6194a9554f6b5400853739
Denys Karpov <Jeremymeme@users.noreply.huggingface.co>
2023-11-08 15:56:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from langchain.llms import OpenAI
+from langchain.document_loaders import Docx2txtLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma, LanceDB
@@ -18,20 +19,11 @@ else:
     openai_api_key = st.text_input('Enter your API key', type='password', key="api_key_input")
     if openai_api_key:
         st.session_state["api_key"] = openai_api_key
-        
-
-if openai_api_key:
-    st.write('<p style="color:green;">API key is being used in the session and will be automatically deleted once the app is closed</p>', unsafe_allow_html=True)
-    #st.empty()
-    
-
-
-
-current_db = None
+        st.write('<p style="color:green;">API key is being used in the session and will be automatically deleted once the app is closed</p>', unsafe_allow_html=True)
+        #st.empty()
 
 
 def generate_embeddings(openai_api_key, uploaded_file):
-    global current_db
     # Load document if file is uploaded
     if uploaded_file is not None and current_db is None:
         file_name = uploaded_file.name
@@ -44,20 +36,19 @@ def generate_embeddings(openai_api_key, uploaded_file):
                 text += page.extract_text()
             text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, length_function=len)
             chunks = text_splitter.split_text(text=text)
-            embeddings = OpenAIEmbeddings(api_key=openai_api_key)
+            embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
             db = Chroma.from_texts(chunks, embeddings)
-    
         elif uploaded_file.type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
             st.write(file_name)
             doc = Document(uploaded_file)
             text = ""
             for para in doc.paragraphs:
                 text += para.text
+                print(para)
             text_splitter = CharacterTextSplitter(separator = "\n\n", chunk_size = 1000, chunk_overlap  = 200, length_function = len, is_separator_regex = False,)
             chunks = text_splitter.split_text(text=text)
-            embeddings = OpenAIEmbeddings(api_key=openai_api_key)
+            embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
             db = Chroma.from_texts(chunks, embeddings)
-            
         else:
             # Extract text from TXT file
             documents = [uploaded_file.read().decode()]
@@ -65,13 +56,11 @@ def generate_embeddings(openai_api_key, uploaded_file):
             text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
             texts = text_splitter.create_documents(documents)
             # Select embeddings
-            embeddings = OpenAIEmbeddings(api_key=openai_api_key)
+            embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
             # Create a vectorstore from documents
             db = Chroma.from_documents(texts, embeddings)
-            
-        current_db = db 
         return db
-
+ 
 
 def generate_response(openai_api_key, query_text):
     global current_db
@@ -82,28 +71,45 @@ def generate_response(openai_api_key, query_text):
         return qa.run(query_text)
     else:
         return "No document uploaded yet."
-            
-            
+
+        
+# Form input and query
+result = []
+
 
 # File upload
 uploaded_file = st.file_uploader('Upload a document', type=['txt', 'pdf', 'docx'])
 # Query text
 query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
 
-# Form input and query
-result = []
+
+# Initialize the Chroma database variable
+@st.cache_data
+def initialize_db():
+    return None
+
+current_db = initialize_db()
+ 
 
 
 if  openai_api_key is not None and current_db is None and uploaded_file is not None:
     st.write("Generating embeddings")
     current_db = generate_embeddings(openai_api_key, uploaded_file)
+    
 
 
-if uploaded_file and query_text:
+if current_db and query_text:
     with st.spinner('Calculating...'):
-        response = generate_response(uploaded_file, user_input, query_text)
+        response = generate_response(openai_api_key, query_text)
         result.append(response)
-        
+
+if current_db is not None:
+    clear_db = st.button('Clear up database from current files')
+    if clear_db:
+        current_db = None
+        result = []
+
+
 
 if len(result):
     st.info(response)
